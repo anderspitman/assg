8124e479027019a86388483d7bfd0ff9a60105b0
Anders Pitman <tapitman11@gmail.com>
2018-07-06 16:27:22 -0700

    Implement projects page
    * Added ability to render HTML directly, instead of forcing
      markdown
    * Extracted a couple functions

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,24 @@ struct Post {
     date: String,
     url: String,
     dir: PathBuf,
+    format: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct ProjectMetadata {
+    title: String,
+    filename: String,
+    format: String,
+    publish: Option<bool>,
+}
+
+#[derive(Debug, Serialize)]
+struct Project {
+    title: String,
+    filename: String,
+    format: String,
+    url: String,
+    dir: PathBuf,
 }
 
 fn main() -> io::Result<()> {
@@ -94,7 +112,8 @@ fn main() -> io::Result<()> {
 
     fs::write(out_dir.join("index.html"), index_string)?;
 
-    make_blog_page(&root_dir, &out_dir, &content_dir, ga_string);
+    make_blog_page(&root_dir, &out_dir, &content_dir, &ga_string)?;
+    make_projects_page(&root_dir, &out_dir, &content_dir, &ga_string)?;
 
     Ok(())
 }
@@ -103,7 +122,7 @@ fn make_blog_page(
     root_dir: &Path,
     out_dir: &Path,
     content_dir: &Path,
-    ga_string: String) -> io::Result<()> {
+    ga_string: &String) -> io::Result<()> {
     
     let blog_root_dir = root_dir.join("blog");
 
@@ -134,10 +153,10 @@ fn make_blog_page(
     // blog index
     for post_dir in post_dirs.into_iter() {
         let metadata = fs::read_to_string(post_dir.join("metadata.toml"))?;
-        let post: PostMetadata = toml::from_str(metadata.as_str()).unwrap();
+        let post_md: PostMetadata = toml::from_str(metadata.as_str()).unwrap();
 
         // skip any posts with false publish metadata
-        if let Some(publish) = post.publish {
+        if let Some(publish) = post_md.publish {
             if !publish {
                 continue;
             }
@@ -148,19 +167,20 @@ fn make_blog_page(
         let mut url_string = url.into_os_string().into_string().unwrap();
         url_string.push('/');
         //println!("{:?}", url);
-        let date = get_date_from_iso8601(&post.date);
+        let date = get_date_from_iso8601(&post_md.date);
         posts.push(Post {
-            title: post.title,
+            title: post_md.title,
             date: date,
             url: url_string,
             dir: post_dir.clone(),
+            format: post_md.format,
         });
     }
 
     posts.sort_unstable_by(|a, b| a.date.cmp(&b.date));
     posts.reverse();
 
-    render_posts(&root_dir, &posts_out_dir, &posts, ga_string.clone())?;
+    render_posts(&root_dir, &posts_out_dir, &posts, &ga_string)?;
 
     let blog_data = MapBuilder::new()
         .insert("posts", &posts).unwrap()
@@ -174,7 +194,7 @@ fn make_blog_page(
     let blog_string = blog_template.render_data_to_string(&blog_data)
         .unwrap();
 
-    fs::write("dist/blog/index.html", blog_string)?;
+    fs::write(blog_out_dir.join("index.html"), blog_string)?;
 
     Ok(())
 }
@@ -183,7 +203,7 @@ fn render_posts(
     content_dir: &Path,
     out_dir: &PathBuf,
     posts: &Vec<Post>,
-    ga_string: String) -> io::Result<()> {
+    ga_string: &String) -> io::Result<()> {
 
     for post in posts {
         let dir = post.dir.clone();
@@ -191,10 +211,9 @@ fn render_posts(
         fs::create_dir(out_dir.join(name))?;
 
         let src_path = dir.join("post.md");
-        let md = fs::read_to_string(src_path)?;
+        let content = fs::read_to_string(src_path)?;
 
-        let renderer = Renderer::new();
-        let html = renderer.render(&md);
+        let html = render_to_html(&content, &post.format);
 
         let post_template = mustache::compile_path(content_dir.join("post.mustache"))
             .expect("Failed to compile post template");
@@ -217,9 +236,134 @@ fn render_posts(
     Ok(())
 }
 
-fn make_projects_page(content_dir: &Path) {
+// TODO: there's a lot of duplicated logic between this and the blog page
+// above.
+fn make_projects_page(
+    root_dir: &Path,
+    out_dir: &Path,
+    content_dir: &Path,
+    ga_string: &String) -> io::Result<()> {
+
+    let projects_root_dir = root_dir.join("projects");
+    let projects_out_dir = out_dir.join("projects");
+    let projects_content_dir = content_dir.join("projects");
+
+    fs::create_dir(&projects_out_dir)?;
+    //fs::create_dir(&posts_out_dir)?;
+   
+    let projects_template = mustache::compile_path(
+        projects_root_dir.join("index.mustache"))
+        .expect("Failed to compile projects template");
+
+    let project_dirs = get_dirs(&projects_content_dir)?;
+
+    let mut projects = Vec::new();
+
+    for project_dir in project_dirs.into_iter() {
+        let metadata = fs::read_to_string(project_dir.join("metadata.toml"))?;
+        let project_md: ProjectMetadata = toml::from_str(metadata.as_str()).unwrap();
+
+        // skip any posts with false publish metadata
+        if let Some(publish) = project_md.publish {
+            if !publish {
+                continue;
+            }
+        }
+
+        let url = Path::new("/projects").join(project_dir.file_name().unwrap());
+        let mut url_string = url.into_os_string().into_string().unwrap();
+        url_string.push('/');
+        projects.push(Project {
+            title: project_md.title,
+            filename: project_md.filename,
+            format: project_md.format,
+            url: url_string,
+            dir: project_dir.clone(),
+        });
+    }
+
+    render_projects(&root_dir, &projects_out_dir, &projects, &ga_string)?;
+
+    let projects_data = MapBuilder::new()
+        .insert("projects", &projects).unwrap()
+        // TODO: this is a hack to workaround the fact that rust-mustache
+        // doesn't seem to be passing the context down to nested partials
+        .insert("ga_partial", &ga_string).unwrap()
+        .build();
+
+    let projects_string = projects_template.render_data_to_string(&projects_data)
+        .unwrap();
+
+    fs::write(projects_out_dir.join("index.html"), projects_string)?;
+
+    Ok(())
+}
+
+fn render_projects(
+    content_dir: &Path,
+    out_dir: &PathBuf,
+    projects: &Vec<Project>,
+    ga_string: &String) -> io::Result<()> {
+
+    for project in projects {
+        let dir = project.dir.clone();
+        let name = dir.file_name().unwrap();
+        fs::create_dir(out_dir.join(name))?;
+
+        let src_path = dir.join(&project.filename);
+        let content = fs::read_to_string(src_path)?;
+
+        let html = render_to_html(&content, &project.format);
+
+        let project_template = mustache::compile_path(content_dir.join("project.mustache"))
+            .expect("Failed to compile project template");
+
+        let project_data = MapBuilder::new()
+           .insert_str("title", project.title.clone())
+           .insert_str("content", html)
+           // TODO: this is a hack to workaround the fact that rust-mustache
+           // doesn't seem to be passing the context down to nested partials
+           .insert("ga_partial", &ga_string).unwrap()
+           .build();
+
+        let project_string = project_template.render_data_to_string(&project_data)
+            .unwrap();
+
+        fs::write(out_dir.join(name).join("index.html"), project_string)?;
+    }
+
+    Ok(())
 }
 
 fn get_date_from_iso8601(iso8601: &String) -> String {
     iso8601.split("T").collect::<Vec<&str>>()[0].to_string()
+}
+
+fn get_dirs(path: &Path) -> io::Result<Vec<PathBuf>> {
+
+    let mut dirs = Vec::new();
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            dirs.push(path);
+        }
+    }
+
+    Ok(dirs)
+}
+
+fn render_to_html(content_string: &String, format: &str) -> String {
+    match format {
+        "markdown" => {
+            let renderer = Renderer::new();
+            renderer.render(&content_string).clone()
+        }
+        "html" => {
+            content_string.clone()
+        }
+        _ => {
+            "<h1>Fail - invalid format</h1>".to_string()
+        }
+    }
 }
